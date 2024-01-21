74b770bfe626f94f776aa14c8d970da9390f4325
kapix9 <61233065+kapix9@users.noreply.github.com>
2024-01-21 19:42:18 +0100

    Update zadanie 1 .py

--- zadanie 1 .py
+++ zadanie 1 .py
@@ -1,16 +1,16 @@
 import base64
 
-def szyfruj_base64(tekst):
+def encrypt_base64(text):
     #Tekst na bajty
-    tekst_bajty = tekst.encode('utf-8')
+    text_bytes = text.encode('utf-8')
     # Szyfrowanie za pomoca base64
-    szyfrogram = base64.b64encode(tekst_bajty)
+    cryptogram = base64.b64encode(text_bytes)
     # Zwracanie szyfrowanego tekstu jako bajty
-    return szyfrogram.decode('utf-8')
+    return cryptogram.decode('utf-8')
 
 # Test 
-tekst =input ("podaj teskt do zaszyfrowania: ")
-print("Tekst do zaszyfrowania:", tekst)
-szyfrogram = szyfruj_base64(tekst)
-print("Zaszyfrowany tekst:", szyfrogram)
-print("Odszyfrowany tekst:", base64.b64decode(szyfrogram).decode('utf-8'))
+text =input ("podaj teskt do zaszyfrowania: ")
+print("Tekst do zaszyfrowania:", text)
+cryptogram = encrypt_base64(text)
+print("Zaszyfrowany tekst:", cryptogram)
+print("Odszyfrowany tekst:", base64.b64decode(cryptogram).decode('utf-8'))
